f8d96be91d2281777fa802fca5324e671e227eb9
Tobias Nemecek <if15b063@technikum-wien.at>
2018-06-20 10:49:29 +0200

    initial project commit

---
+++ CI-Test/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1cdab8bc-f2c9-4744-886c-49c5eea8a954")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CI-Test/Tests/UnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CI_Test;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void Calculator_Add_Success()
+        {
+            Calculator calc = new Calculator();
+            int result = calc.Add(1, 2);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
